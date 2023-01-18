e6a001c2747e5bd8a992a712da332e5c8936996d
matkwiecien <m.kwiecien916@gmail.com>
2023-01-18 14:55:47 +0100

    Configure prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-
 function App() {
-
-  return (
-    <div>
-        test
-    </div>
-  )
+  return <div>test</div>;
 }
 
-export default App
+export default App;
