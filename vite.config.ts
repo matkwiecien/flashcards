61b8cf7e609b0bdfbab08c1bb3da636998f93183
matkwiecien <m.kwiecien916@gmail.com>
2023-01-18 14:55:47 +0100

    Init project

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+    VitePWA(
+      {  
+        registerType: 'autoUpdate',
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'my-sw.js' 
+      })
+],
+})
+
